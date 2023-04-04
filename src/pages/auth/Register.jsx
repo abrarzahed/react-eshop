f68b0dbd0e8e432becb8d7aaab725817fd21eaae
abrarzahed <abrarzahed6986@gmail.com>
2023-04-04 16:31:26 +0600

    register page markup

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,4 +1,32 @@
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
 import styles from "./Auth.module.scss";
+import registerImage from "../../assets/register.png";
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <Card cardClass="">
+        <div className={styles.form}>
+          <h2>Register</h2>
+          <form>
+            <input type="text" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <input type="password" placeholder="Confirm Password" required />
+            <button className="--btn --btn-primary --btn-block">
+              Register
+            </button>
+          </form>
+
+          <span className={styles.register}>
+            <p>Already have an account? </p>
+            <Link to="/login"> Login </Link>
+          </span>
+        </div>
+      </Card>
+      <div className={styles.img}>
+        <img src={registerImage} alt="register" width={400} />
+      </div>
+    </section>
+  );
 }
